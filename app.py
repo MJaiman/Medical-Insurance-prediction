3e11b7ba8a10c2ca21ee06e2e5dc2c40281304a3
MJaiman <67483284+MJaiman@users.noreply.github.com>
2022-07-26 14:53:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import numpy as np
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
 
-Pkl_Filename = "rf_tuned.pkl" 
-with open(Pkl_Filename, 'rb') as file:  
-    model = pickle.load(file)
+# Pkl_Filename = "rf_tuned.pkl" 
+# with open(Pkl_Filename, 'rb') as file:  
+#     model = pickle.load(file)
 @app.route('/')
 
 def hello_world():
